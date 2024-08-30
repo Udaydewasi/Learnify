9c3b829eed71aec611a8d0f11838efa8b2157e7a
Uday dewasi <143155466+Udaydewasi@users.noreply.github.com>
2024-08-30 19:18:13 +0530

    Update Footer.jsx
    name uday dewasi

--- client/src/components/Common/Footer.jsx
+++ client/src/components/Common/Footer.jsx
@@ -172,7 +172,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made with ❤️ By Aakash Bhargava © 2023 Learnify</div>
+          <div className="text-center">Made with ❤️ By Uday Dewasi © 2023 Learnify</div>
         </div>
       </div>
     </div>
