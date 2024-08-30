799879db3cb19a205618df3c918013cc1207561b
Uday dewasi <143155466+Udaydewasi@users.noreply.github.com>
2024-08-30 19:46:23 +0530

    Update apis.js
    removed /

--- client/src/services/apis.js
+++ client/src/services/apis.js
@@ -3,15 +3,15 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 // AUTH ENDPOINTS
 export const endpoints = {
-  SENDOTP_API: BASE_URL + "/auth/sendotp",  
-  SIGNUP_API: BASE_URL + "/auth/signup",
+  SENDOTP_API: BASE_URL + "auth/sendotp",  
+  SIGNUP_API: BASE_URL + "auth/signup",
   LOGIN_API: BASE_URL + "auth/login",
   RESETPASSTOKEN_API: BASE_URL + "auth/reset-password-token",
   RESETPASSWORD_API: BASE_URL + "auth/reset-password",
 }
-console.log(BASE_URL + "/auth/sendotp");
-console.log(BASE_URL + "/auth/signup");
-console.log(BASE_URL + "auth/login");
+// console.log(BASE_URL + "/auth/sendotp");
+// console.log(BASE_URL + "/auth/signup");
+// console.log(BASE_URL + "auth/login");
 // PROFILE ENDPOINTS
 export const profileEndpoints = {
   GET_USER_DETAILS_API: BASE_URL + "profile/getUserDetails",
